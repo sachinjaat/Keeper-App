941451aa4f10b1d411ce2382ba249853e28dd0d7
sachin choudhary <sachinbarkhera1999@gmail.com>
2020-07-26 12:02:47 +0530

    webpack config. issue resolved

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {App} from "./views/App";
+import "./public/styles.css";
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+
+
+ReactDOM.render(<App />,document.getElementById('root')
+);
