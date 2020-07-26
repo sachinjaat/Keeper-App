7c5f67e08cbfadb5d2902b67aac3ec6a50718f50
sachin choudhary <sachinbarkhera1999@gmail.com>
2020-07-26 13:50:10 +0530

    Project with create-react-app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {App} from "./App";
+import "./styles.css";
+// import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+
+
+ReactDOM.render(<App />,document.getElementById('root')
+);
